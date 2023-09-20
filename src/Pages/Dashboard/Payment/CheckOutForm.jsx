8df659651f3df4473d87cf53c5d6aa27abf2e584
smmazharu <hmoxygen@gmail.com>
2023-09-20 16:21:02 +0600

    make database by payment and intigration with clientside

--- src/Pages/Dashboard/Payment/CheckOutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -1,8 +1,27 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
-const CheckOutForm = () => {
+const CheckOutForm = ({ cart, price }) => {
     const stripe=useStripe();
-    const elements=useElements()
+    const elements=useElements();
+    const {user}=useAuth()
+    const [axiosSecure]=useAxiosSecure();
+    const [cardError,setCardError]=useState('');
+    const [clientSecret,setClientSecret]=useState('');
+    const [processing,setProcessing]=useState(false);
+    const [transactionId,setTransactionId]=useState('');
+    useEffect(()=>{
+        axiosSecure.post('/create-payment-intent',{price})
+        .then(res=>{
+            // console.log(res.data.clientSecret)
+            setClientSecret(res.data.clientSecret);
+        })
+    },[price,axiosSecure])
+
+
+
     const handleSubmit=async(event)=>{
         event.preventDefault();
         if(!stripe || !elements){
@@ -12,9 +31,65 @@ const CheckOutForm = () => {
         if(card===null){
             return
         }
+        console.log("card",card)
+
+        const {error, paymentMethod}=await stripe.createPaymentMethod({
+            type:'card',
+            card
+        })
+        if(error){
+            console.log("error", error)
+            setCardError(error)
+        }else{
+            setCardError('')
+            console.log('payment method',paymentMethod)
+           
+        }
+
+        setProcessing(true)
+
+        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card: card,
+                billing_details: {
+                  email: user?.email || 'unknown',
+                  name:user?.displayName || 'unknown'
+                },
+              },
+            },
+          );
+
+            if(confirmError){
+                console.log(confirmError)
+                // setCardError()
+            }
+            console.log(paymentIntent)
+            setProcessing(false)
+            if (paymentIntent.status === 'succeeded') {
+                setTransactionId(paymentIntent.id);
+                // save payment information to the server
+                const payment = {
+                    email: user?.email,
+                    transactionId: paymentIntent.id,
+                    price,
+                    // quantity: cart?.length,
+                    cartItems: cart?.map(item => item?._id),
+                    itemNames: cart?.map(item => item?.name)
+                }
+                axiosSecure.post('/payments', payment)
+                    .then(res => {
+                        console.log('payment ',res.data);
+                        // if (res.data.result.insertedId) {
+                            
+                        // }
+                    })
+            }
     }
     return (
-        <form onSubmit={handleSubmit}>
+      < >
+        <form onSubmit={handleSubmit}className="w-2/3 m-8" >
       <CardElement
         options={{
           style: {
@@ -31,10 +106,15 @@ const CheckOutForm = () => {
           },
         }}
       />
-      <button type="submit" disabled={!stripe}>
+      <button className="btn  btn-primary mt-4 btn-sm" type="submit" disabled={!stripe || !clientSecret || processing}>
         Pay
       </button>
     </form>
+        {cardError && 
+        <p className="text-red-600 ml-10" >{cardError.message}</p>
+        }
+        {transactionId && <p className="text-green-500">Transaction complete with TransactionID: {transactionId}</p>}
+      </>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -2,17 +2,21 @@ import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import CheckOutForm from "./CheckOutForm";
 import { loadStripe } from "@stripe/stripe-js";
+import useCart from "../../../hooks/useCart";
 
 
 //TODO: provide publishable key
+const stripePromise=loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
 const Payment = () => {
-    const stripePromise=loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK)
+    const [cart]=useCart();
+    const total= cart.reduce((sum,item)=>sum+item.price,0);
+    const price=parseFloat(total.toFixed(2))
     return (
         <div className="w-full">
             <SectionTitle heading={"Payment"} subHeading={'Please process'}></SectionTitle>
            <h1 className="text-3xl" >PAYMENT</h1>
             <Elements stripe={stripePromise}>
-                <CheckOutForm></CheckOutForm>
+                <CheckOutForm price={price} cart={cart}></CheckOutForm>
             </Elements>
         </div>
     );
