ce281a1b237292ad1c3fd4263c334ab124bdb0be
smmazharu <hmoxygen@gmail.com>
2023-10-20 09:07:39 +0600

    sidebar modify in dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -16,16 +16,16 @@ const Dashboard = () => {
     // const isAdmin=true;
     const [isAdmin]=useAdmin();
   return (
-    <div className="drawer drawer-mobile ">
+    <div className="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
     <div className="drawer-content flex flex-col items-center justify-center">
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-        <Outlet></Outlet>
+    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                <Outlet></Outlet>
 
-    </div>
-    <div className="drawer-side bg-[#D1A054]">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80">
+            </div>
+     <div className="drawer-side ">
+    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 ">
           {
             isAdmin? <>
             <li>
@@ -95,7 +95,11 @@ const Dashboard = () => {
          
         </ul>
       </div>
+
+
+
     </div>
+    
   );
 };
 
